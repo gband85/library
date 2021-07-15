91a18684b05368bf6ceffafff915f13096ea0f29
Garrett Anderson <keaton85@runbox.com>
2021-07-15 13:32:36 -0500

    display books on page

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 let myLibrary = [];
+let display = document.querySelector(".display");
 
-function Book(title,author,pages,read) {
-    this.title=title;
-    this.author=author;
-    this.pages=pages;
-    this.read=read ? "read" : "not read yet";
-    this.info=function() {
-        return `${title} by ${author}, ${pages} pages, ${read}`; 
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read ? "read" : "not read yet";
+    this.info = function () {
+        return `${title} by ${author}, ${pages} pages, ${read ? "read" : "not read yet"}`;
     }
 }
 
-function addBookToLibrary(title,author,pages,read) {
-let bookToAdd=new Book(title,author,pages,read);
-myLibrary.push(bookToAdd);
+function addBookToLibrary(title, author, pages, read) {
+    let bookToAdd = new Book(title, author, pages, read);
+    myLibrary.push(bookToAdd);
 }
 
-addBookToLibrary("Candide","Voltaire",129,false);
-addBookToLibrary("The Prince","Niccolo Machiavelli",140,true);
-addBookToLibrary("Altered Carbon","Richard K. Morgan",375,true);
-addBookToLibrary("The Hitchhiker's Guide to the Galaxy","Douglas Adams",193,true);
+addBookToLibrary("Candide", "Voltaire", 129, false);
+addBookToLibrary("The Prince", "Niccolo Machiavelli", 140, true);
+addBookToLibrary("Altered Carbon", "Richard K. Morgan", 375, true);
+addBookToLibrary("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 193, true);
+displayBooks();
 
 function displayBooks() {
-    myLibrary.forEach(function(elem){
-console.log(elem);
+    myLibrary.forEach(function (elem) {
+        console.log(elem);
+        let card = document.createElement("div");
+        card.classList.add("card");
+        card.textContent=elem.info();
+        display.appendChild(card);
     });
 }
