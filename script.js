25d41c40ac849c8124d31ca4af385f3b83ec443d
Garrett Anderson <keaton85@runbox.com>
2021-07-16 16:37:56 -0500

    add button listener

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let myLibrary = [];
 let display = document.querySelector(".display");
+let newBook = document.querySelector("#new-book");
+let bookForm = document.querySelector("#book-form");
+let title = document.querySelector("#title");
+let author = document.querySelector("#author");
+let pages = document.querySelector("#pages");
+let read = document.querySelector("#read");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -12,22 +18,35 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(title, author, pages, read) {
+    //create book
     let bookToAdd = new Book(title, author, pages, read);
+    //add to library array
     myLibrary.push(bookToAdd);
 }
+
+function displayBooks() {
+    myLibrary.forEach(function (elem) {
+        console.log(elem);
+        //create card element
+        let card = document.createElement("div");
+        //add class
+        card.classList.add("card");
+        //add book data
+        card.textContent = elem.info();
+        //append to display
+        display.appendChild(card);
+    });
+}
+
+bookForm.addEventListener("submit", function (e) {
+    //prevent default submit action
+    e.preventDefault();
+    //add to library
+    addBookToLibrary(title.value, author.value, Number(pages.value), read.value);
+})
 
 addBookToLibrary("Candide", "Voltaire", 129, false);
 addBookToLibrary("The Prince", "Niccolo Machiavelli", 140, true);
 addBookToLibrary("Altered Carbon", "Richard K. Morgan", 375, true);
 addBookToLibrary("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 193, true);
 displayBooks();
-
-function displayBooks() {
-    myLibrary.forEach(function (elem) {
-        console.log(elem);
-        let card = document.createElement("div");
-        card.classList.add("card");
-        card.textContent=elem.info();
-        display.appendChild(card);
-    });
-}
